5942e19a9a963ba99caa1f5fd048ce48314ea691
Franciom Veloso <franciomveloso@gmail.com>
2021-07-16 21:34:27 -0300

    atualizado modificadores de acesso cpf function

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,5 +1,17 @@
 package one.digitalinnovation.digionebank
 
 class Pessoa {
+    var nome:String = "Jether"
+    var cpf:String = "123.123.123.12"
+    private set
+
+}
+
+fun main(){
+    val jether = Pessoa()
+    //variável recebe a instância da classe pessoa.
+
+    println(jether.nome)
+    println(jether.cpf)
 
 }

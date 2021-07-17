96216a017aa7e93fbcfdfc68cdbec51a30dd8e21
Franciom Veloso <franciomveloso@gmail.com>
2021-07-16 21:43:26 -0300

    atualizado membros da classe

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -5,13 +5,16 @@ class Pessoa {
     var cpf:String = "123.123.123.12"
     private set
 
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
+
 }
 
 fun main(){
     val jether = Pessoa()
     //variável recebe a instância da classe pessoa.
 
-    println(jether.nome)
-    println(jether.cpf)
+    println(jether.pessoaInfo())
 
 }

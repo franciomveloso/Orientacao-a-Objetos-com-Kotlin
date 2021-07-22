18d7cd92b1994cb48da8e56010bb6d81f2eb82d5
Franciom Veloso <franciomveloso@gmail.com>
2021-07-22 19:00:41 -0300

    atualizado classe pessoa e criado classe abstrata

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,20 +1,8 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome:String = "Jether"
-    var cpf:String = "123.123.123.12"
-    private set
+abstract class Pessoa (
+    val nome: String,
+    val cpf: String
+)
 
-    constructor()
 
-    fun pessoaInfo() = "$nome e $cpf"
-
-}
-
-fun main(){
-    val jether = Pessoa()
-    //variável recebe a instância da classe pessoa.
-
-    println(jether.pessoaInfo())
-
-}

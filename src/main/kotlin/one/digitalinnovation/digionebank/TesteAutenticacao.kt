d079f7636d1b286998d8c7168734cd566e8d192b
Franciom Veloso <franciomveloso@gmail.com>
2021-07-26 15:15:11 -0300

    criado teste autenticação e cliente

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/TesteAutenticacao.kt
@@ -0,0 +1,8 @@
+package one.digitalinnovation.digionebank
+
+import one.digitalinnovation.digionebank.Logavel
+
+class TesteAutenticacao {
+    fun autentica(logavel: Logavel) = println(logavel.login())
+
+}
---
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteCliente.kt
@@ -0,0 +1,19 @@
+package one.digitalinnovation.digionebank.testes
+
+import one.digitalinnovation.digionebank.Cliente
+import one.digitalinnovation.digionebank.ClienteTipo
+import one.digitalinnovation.digionebank.TesteAutenticacao
+
+fun main(){
+    val jose = Cliente(
+        nome = "Jose da Silva ",
+        cpf = "123.123.123-45",
+        clienteTipo = ClienteTipo.PF,
+        senha = "123456"
+    )
+
+    println(jose)
+
+    TesteAutenticacao().autentica(jose)
+
+}
